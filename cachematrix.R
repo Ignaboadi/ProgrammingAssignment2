4d4fca70ee398ea9819259e9d2fb4ee4c3b1cfd1
Ignaboadi <ignatius@aims.edu.gh>
2020-12-30 09:41:51 +0000

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -3,30 +3,33 @@
 
 ## Write a short comment describing this function
 
+##This function takes in a matrix and sets the inverse if it is already computed and to NULL
+##if it is not computed yet. The matrix should be non-singular. 
 makeCacheMatrix <- function(x = matrix()) { #argument is a non-singular matrix
-    inv_matrix <- NULL  
+    inv_matrix <- NULL  #set inverse to NULL since it has not been computed yet
     set <- function(y){
         x <<- y
         inv_matrix <<- NULL
     }
-    get <- function() {x}
-    setinverse <- function(inverse) {inv_matrix <<- inverse}
-    getinverse <- function() {inv_matrix}
+    get <- function() {x} #outputs the matrix
+    setinverse <- function(inverse) {inv_matrix <<- inverse} #sets the inverse after it has been computed
+    getinverse <- function() {inv_matrix}    #outputs the inverse
     list(set = set, get = get,setinverse = setinverse,getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+##This function computes the inverse of the matrix if it is not already computed 
+##and adds it to the attributes so that it is extracted when it is needed again.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    inv_matrix <- x$getinverse()
-    if(!is.null(inv_matrix)) {
-        message("getting cached data")
-        return(inv_matrix)
+    inv_matrix <- x$getinverse() #extracts the inverse of the matrix
+    if(!is.null(inv_matrix)) { #if the inverse has been computed already
+        message("getting cached data") #lets the user know it has been computed already 
+        return(inv_matrix) #outputs the inverse
     }
-    matrix <- x$get()
-    inv_matrix <- solve(matrix, ...)
-    x$setinverse(inv_matrix)
-    inv_matrix
+    matrix <- x$get() #outputs the matrix
+    inv_matrix <- solve(matrix, ...)  #computes the inverse
+    x$setinverse(inv_matrix) #change the inverse from NULL to the computed inverse
+    inv_matrix  
 }
