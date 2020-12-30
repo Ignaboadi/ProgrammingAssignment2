d9be75401976913677a1441b5d6f9c5baf7808fd
Ignaboadi <ignatius@aims.edu.gh>
2020-12-30 09:23:42 +0000

    submission for peer-review assessment

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { #argument is a non-singular matrix
+    inv_matrix <- NULL  
+    set <- function(y){
+        x <<- y
+        inv_matrix <<- NULL
+    }
+    get <- function() {x}
+    setinverse <- function(inverse) {inv_matrix <<- inverse}
+    getinverse <- function() {inv_matrix}
+    list(set = set, get = get,setinverse = setinverse,getinverse = getinverse)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv_matrix <- x$getinverse()
+    if(!is.null(inv_matrix)) {
+        message("getting cached data")
+        return(inv_matrix)
+    }
+    matrix <- x$get()
+    inv_matrix <- solve(matrix, ...)
+    x$setinverse(inv_matrix)
+    inv_matrix
 }
